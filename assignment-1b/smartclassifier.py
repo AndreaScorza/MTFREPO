32feb9f92204f78177d85a434164d77067e66503
Luka <lukavdplas@gmail.com>
2019-09-24 10:54:18 +0200

    added comments and header

--- assignment-1b/smartclassifier.py
+++ assignment-1b/smartclassifier.py
@@ -1,5 +1,10 @@
+"""
+Jos Hens (5737222), Luka van der Plas (4119142), Andrea Scorza (6649173)
+Methods in AI Research
+Assignment 1b
+"""
+
 import numpy as np
-import sklearn as sk
 from sklearn import tree
 
 #paths
@@ -9,11 +14,13 @@ trainpath = './trainData.txt' #location of training data
 
 trainfile = open(trainpath, 'r')
 data = trainfile.readlines()
+#split into train and dev set with 80/20 ratio
 ratio = 0.8
 traindata, devdata = data[:int(len(data)*ratio)], data[int(len(data)*(1-ratio)):]
 trainfile.close()
 
 def parseExample(line):
+    # split a line into a tuple with the tag and the sentence
     words = line.split()
     tag = words[0]
     sentence = " ".join(words[1:])
@@ -21,6 +28,7 @@ def parseExample(line):
 
 #get a dictionary with all words in the training corpus
 
+#we're going to sort words by their frequency, so feqdict lists the absolute frequency for each word
 freqdict = dict()
 tags = set()
 
@@ -29,8 +37,10 @@ for line in traindata:
     tags.add(tag)
     for word in sentence.split():
         if word in freqdict:
+            #increase count for this word
             freqdict[word] += 1
         else:
+            #otherwise, add word to dictionary with count 1
             freqdict[word] = 1
 
 vocab = []
@@ -57,7 +67,7 @@ def bagofwords(sentence):
         vector[index] += 1
     return vector
 
-# get tags
+# convert tags to integers and vice versa
 
 taglist = list(tags)
 
@@ -82,16 +92,10 @@ for line in traindata:
     output.append(tagindex)
 
 # train a decision tree
+
 print('training...')
 model = sk.tree.DecisionTreeClassifier()
 model.fit(train_input, output)
-
-# try a sentence
-#sent = 'hello'
-#sentvector = bagofwords(sent)
-#prediction = model.predict([sentvector])
-#print(sent)
-#print('prediction:', indexToTag(prediction[0]))
 
 # evaluate the model
 
@@ -119,6 +123,8 @@ print('done!')
 print('accuracy ratio:', accuracy)
 print()
 print('Try it yourself!')
+
+# classify user iput
 
 while True:
     userinput = input()
